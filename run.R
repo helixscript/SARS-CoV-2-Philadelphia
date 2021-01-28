48830d0e359a3a382372da5fc9d48128ad339e2a
John Everett <everett.jk@gmail.com>
2021-01-27 19:34:09 -0500

    restructuring

--- run.R
+++ run.R
@@ -2,3 +2,4 @@ Rscript <- '/home/opt/R-3.4.0/bin/Rscript'
 system(paste0(Rscript, ' build_VSP_data.R'))
 system(paste0(Rscript, ' build_reports.R'))
 system(paste0(Rscript, ' build_trial_tables.R'))
+system(paste0(Rscript, ' allGenomes.R'))
